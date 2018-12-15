22f76fa556d659ce3fc408180f8edff2f807de8d
Hluhov Max <holosievo@gmail.com>
2018-12-15 16:52:19 +0200

    added controller & view layer

--- src/main/java/mvc/service/ProjectService.java
+++ src/main/java/mvc/service/ProjectService.java
@@ -1,4 +1,32 @@
 package mvc.service;
 
+import mvc.dao.ProjectRepo;
+import mvc.model.Project;
+
+import java.util.List;
+
 public class ProjectService {
+    private ProjectRepo projectRepo = new ProjectRepo();
+
+    public void saveToProjectRepo(Project project) {
+        projectRepo.save(project);
+    }
+
+    public Project getByIdFromProjectRepo(Integer id) {
+        Project project = projectRepo.getById(id);
+        return project;
+    }
+
+    public void updateToProjectRepo(Project project) {
+        projectRepo.update(project);
+    }
+
+    public List<Project> getAllFromProjectRepo() {
+        List<Project> projects = projectRepo.getAll();
+        return projects;
+    }
+
+    public void deleteFromProjectRepo(Integer id) {
+        projectRepo.delete(id);
+    }
 }
--- src/main/java/mvc/service/SkillService.java
+++ src/main/java/mvc/service/SkillService.java
@@ -1,4 +1,32 @@
 package mvc.service;
 
+import mvc.dao.SkillRepo;
+import mvc.model.Skill;
+
+import java.util.List;
+
 public class SkillService {
+    private SkillRepo skillRepo = new SkillRepo();
+
+    public void saveToSkillRepo(Skill skill) {
+        skillRepo.save(skill);
+    }
+
+    public Skill getByIdFromSkillRepo(Integer id) {
+        Skill skill = skillRepo.getById(id);
+        return skill;
+    }
+
+    public void updateToSkillRepo(Skill skill) {
+        skillRepo.update(skill);
+    }
+
+    public List<Skill> getAllFromProjectRepo() {
+        List<Skill> skills = skillRepo.getAll();
+        return skills;
+    }
+
+    public void deleteFromSkillRepo(Integer id) {
+        skillRepo.delete(id);
+    }
 }
